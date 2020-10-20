8391f6020fb1037458ebb1ed4cf49e2c93186a2a
Josh Allan <allan.josh07@gmail.com>
2020-10-20 14:35:00 -0400

    Began building the Employee constructor and prototypes for the employee class.

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,12 +1,35 @@
 // TODO: Write code to define and export the Employee class
-const employee = {
-	name: this.name,
-	id: this.id,
-	email: this.email,
-	getName: () => this.name,
-	getID: () => this.ID,
-	getEmail: () => this.email,
-	getRole: () => "Employee",
+
+// Create the constructor for the class
+function Employee(name, id, email, getName, getID, getEmail, getRole) {
+	(this.name = name),
+		(this.id = id),
+		(this.email = email),
+		(this.getName = getName()),
+		(this.getID = getID()),
+		(this.getEmail = getEmail()),
+		(this.getRole = getRole());
+}
+
+// Define the getName prototype
+Employee.prototype.getName() = function () {
+	return this.name;
 };
 
-module.exports = employee;
+// Define the getID prototype
+Employee.prototype.getID() = function () {
+	return this.id;
+};
+
+// Define the getEmail prototype
+Employee.prototype.getEmail() = function () {
+	return this.email;
+};
+
+// Define the getRole prototype
+Employee.prototype.getRole() = function () {
+	return "Employee";
+};
+
+// Export the class
+module.exports = Employee;
