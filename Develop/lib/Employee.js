027707aad1fdcb62844eec9feac9ea57d04dcc7b
Josh Allan <allan.josh07@gmail.com>
2020-10-20 15:38:35 -0400

    Adjusted Employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,35 +1,28 @@
 // TODO: Write code to define and export the Employee class
-
-// Create the constructor for the class
-function Employee(name, id, email, getName, getID, getEmail, getRole) {
-	(this.name = name),
-		(this.id = id),
-		(this.email = email),
-		(this.getName = getName()),
-		(this.getID = getID()),
-		(this.getEmail = getEmail()),
-		(this.getRole = getRole());
+// define the class object
+class Employee {
+	// create the main constructor for the employee class
+	constructor(name, id, email) {
+		this.name = name;
+		this.id = id;
+		this.email = email;
+	}
+	// define the getName function
+	getName() {
+		return this.name;
+	}
+	// define the getID function
+	getID() {
+		return this.id;
+	}
+	// define the getEmail function
+	getEmail() {
+		return this.email;
+	}
+	// define the getRole function
+	getRole() {
+		return "Employee";
+	}
 }
-
-// Define the getName prototype
-Employee.prototype.getName() = function () {
-	return this.name;
-};
-
-// Define the getID prototype
-Employee.prototype.getID() = function () {
-	return this.id;
-};
-
-// Define the getEmail prototype
-Employee.prototype.getEmail() = function () {
-	return this.email;
-};
-
-// Define the getRole prototype
-Employee.prototype.getRole() = function () {
-	return "Employee";
-};
-
-// Export the class
+// Export the class so that we can access it in other files
 module.exports = Employee;
