3db77c809098c96cf45fd36ab6776c2c59b71114
Josh Allan <allan.josh07@gmail.com>
2020-10-21 13:59:44 -0400

    Built out the manager function to collect their information

--- Develop/app.js
+++ Develop/app.js
@@ -11,7 +11,88 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// Function that gets the information of the manager of the team
+function getManager() {
+	const intro = [
+		"\r",
+		"-".repeat(60),
+		"Please answer the following questions to build your team.",
+		"First, let's get information about the manager.",
+	].join("\n\n");
+	console.log(intro + "\n");
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				message: "What is the manager's name?",
+				name: "managerName",
+				validate: (input) => {
+					if (input !== "") {
+						return true;
+					} else {
+						return "Please enter a name for your manager.";
+					}
+				},
+			},
+			{
+				type: "input",
+				name: "managerId",
+				message: "What is the manager's ID number?",
+				validate: (input) => {
+					const passed = input.match(/^[1-9]\d*$/);
+					if (passed) {
+						return true;
+					}
+					return "Please enter a positive number greater than zero.";
+				},
+			},
+			{
+				type: "input",
+				name: "managerEmail",
+				message: "What is the manager's email address?",
+				validate: (input) => {
+					const passed = input.match(/\S+@\S+\.\S+/);
+					if (passed) {
+						return true;
+					}
+					return "Please enter a valid email address.";
+				},
+			},
+			{
+				type: "input",
+				name: "managerOffice",
+				message: "What is the manager's office number?",
+				validate: (input) => {
+					const passed = input.match(/^[1-9]\d*$/);
+					if (passed) {
+						return true;
+					}
+					return "Please enter numbers only.";
+				},
+			},
+		])
+		// and to create objects for each team member (using the correct classes as blueprints!)
+		.then((responses) => {
+			const manager = new Manager(
+				responses.managerName,
+				responses.managerId,
+				responses.managerEmail,
+				responses.managerOffice
+			);
+			teamMembers.push(manager);
+			console.log(teamMembers);
+			addMembers();
+		});
+}
+
+// function that asks to create more seats for additional team members (engineers and interns)
+function addMembers() {}
+
+// store the team members in an array of objects after they have been created
+const teamMembers = [];
+
+// initialize the getManager function
+getManager();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
